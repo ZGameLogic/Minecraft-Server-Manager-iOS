0a4097fb762d8c2ba8e4a125b58fda38a5a48d18
Ben Shabowski <benshabowski@gmail.com>
2026-05-28 18:50:33 -0500

    Initial Commit

---
+++ Minecraft Server Manager/Minecraft_Server_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minecraft_Server_ManagerApp.swift
+//  Minecraft Server Manager
+//
+//  Created by Ben Shabowski on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Minecraft_Server_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
